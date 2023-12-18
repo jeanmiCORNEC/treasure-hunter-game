8ed5d74d39253f887f52de370bd111e3c44af8cb
jeanmiCORNEC <maelgut@hotmail.fr>
2023-12-19 00:54:36 +0100

    redemarage sur pc neuf

---
+++ lib/utils/palette.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class Palette {
+  static const Color neonOrange = Color(0xFFFF3D00);
+  static const Color cyan = Color(0xFF00FFFF);
+  static const Color magenta = Color(0xFFFF00FF);
+  static const Color limeGreen = Color(0xFF00FF00);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color deepBlue = Color(0xFF36344f);
+  static const Color warmAccent = Color(0xFFda571c);
+  static const Color softBeige = Color(0xFFe2dacb);
+  static const Color darkAccent = Color(0xFF753812);
+  static const Color mutedYellow = Color(0xFFaba456);
+
+  // Getters pour chaque couleur
+  static Color get neonOrangeColor => neonOrange;
+  static Color get cyanColor => cyan;
+  static Color get magentaColor => magenta;
+  static Color get limeGreenColor => limeGreen;
+  static Color get whiteColor => white;
+  static Color get deepBlueColor => deepBlue;
+  static Color get warmAccentColor => warmAccent;
+  static Color get softBeigeColor => softBeige;
+  static Color get darkAccentColor => darkAccent;
+  static Color get mutedYellowColor => mutedYellow;
+}
